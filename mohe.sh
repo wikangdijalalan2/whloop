5f7b3b28398d34d73d4c990e96ce48318db8454f
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-06-04 16:33:18 +0800

    Update mohe.sh

--- mohe.sh
+++ mohe.sh
@@ -2,7 +2,16 @@
 
 # This generates a file every 5 minutes
 
-while true; do
+: ' while true; do
 touch pic-`date +%s`.jpg
 sleep 300
+done '
+
+
+i=360000000000000000
+
+while [ $i -le 2 ]
+do
+  echo Number: $i
+  ((i++))
 done
