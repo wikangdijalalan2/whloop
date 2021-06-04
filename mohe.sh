1eacea3f451bf77d848acca818b04dfd53c913c9
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-06-04 17:06:09 +0800

    Update mohe.sh

--- mohe.sh
+++ mohe.sh
@@ -7,10 +7,11 @@
 #sleep 300
 #done '
 
-timer=$(shuf -i 10-20 -n 1)
+
 
 while :
 do
+        timer=$(shuf -i 10-20 -n 1)
 	echo "infinite loops [ hit CTRL+C to stop]"
 	sleep "$timer"
 	touch pic-`date +%s`.jpg
